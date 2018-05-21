d3f530759d511fe6ccec737e67a79514f466f257
Vaibhav Bali <vbali@internode.on.net>
2018-05-21 13:45:59 +1000

    Added Logging

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
    public class SamuraiContext : DbContext
    {
+      public static readonly LoggerFactory MyConsoleLoggerFactory = new LoggerFactory(
+         new []
+         {
+            new ConsoleLoggerProvider((category, level) => category == DbLoggerCategory.Database.Command.Name
+            && level == LogLevel.Information, true) 
+         });
+
       public DbSet<Samurai> Samurais { get; set; }
       public DbSet<Battle> Battles { get; set; }
       public DbSet<Quote> Quotes { get; set; }
 
       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
       {
-         optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb;Database = SamuraiApp;Trusted_Connection = True;");
+         optionsBuilder
+            .UseLoggerFactory(MyConsoleLoggerFactory)
+            .UseSqlServer("Server = (localdb)\\mssqllocaldb;Database = SamuraiApp;Trusted_Connection = True;");
          
       }
 
