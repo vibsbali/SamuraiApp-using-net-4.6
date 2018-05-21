6be0a41ccc98621073c29c4fd2cd3c0a9a7656e0
Vaibhav Bali <vbali@internode.on.net>
2018-05-21 12:37:45 +1000

    Inital

---
+++ SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,13 @@
+﻿namespace SamuraiApp.Domain
+{
+   public class Quote
+   {
+      public int Id { get; set; }
+      public string Text { get; set; }
+
+      //Navigation Property
+      public Samurai Samurai { get; set; }
+      //Foreign Id
+      public int SamuraiId { get; set; }
+   }
+}
---
+++ SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+   public class Samurai
+   {
+      public int Id { get; set; }
+      public string Name { get; set; }
+
+      //Foreign Id
+      public int BattleId { get; set; }
+      public List<Quote> Quotes { get; set; } = new List<Quote>();
+   }
+}
